1078ad3aafb9701e1b92a492fa2152d066333125
algo1-2 <rivasbenjamin7214@gmail.com>
2026-09-15 15:29:47 -0300

    feat(contacto): desarrollo de aplicación contacto con formulario y sección de preguntas frecuentes

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6h*e&m()@b&ka(f#1nl-9m#e&#c^oxbzs65@57++&*ot*g5^un
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contacto',
 ]
 
 MIDDLEWARE = [
---
+++ contacto/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path
+from . import views
+
+app_name = 'contacto'
+
+urlpatterns = [
+    path('', views.formulario, name='formulario'),
+    path('faq/', views.faq, name='faq'),
+]
